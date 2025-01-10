0765df9a9c3c5a76a0622c266ee39f7077def1ef
Rover <natrajv@yahoo.com>
2025-01-10 10:38:20 +0530

    Deleted PS files
    Deleted all PS files which is not related to CS

--- test1.ps1
+++
@@ -1,7 +0,0 @@
-$sFiles=Get-ChildItem -File -filter *.txt
-Clear-Host
-foreach ($sFile in $sFiles) {
-    [string] $sFile1 = $sFile
-    $sFile1 = $sFile1.ToUpper()
-    Rename-Item $sFile $sFile1 -WhatIf
-}
